52a4fbe650673b6f392f685caa8a9e08ff33bda1
yourboyfred <fredrickkimathi26@gmail.com>
2024-06-03 16:32:20 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('hamburger-icon').addEventListener('click', function() {
+    document.getElementById('off-canvas-menu').style.display = 'block';
+});
+
+document.getElementById('close-btn').addEventListener('click', function() {
+    document.getElementById('off-canvas-menu').style.display = 'none';
+});
